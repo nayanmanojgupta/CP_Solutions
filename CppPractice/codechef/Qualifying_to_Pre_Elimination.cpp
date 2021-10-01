7af8640a6ea899d88697ead11cb419575d15d695
UnnatKr <79165109+UnnatKr@users.noreply.github.com>
2021-10-01 13:29:51 +0530

    Update Qualifying_to_Pre_Elimination.cpp

--- CppPractice/codechef/Qualifying_to_Pre_Elimination.cpp
+++ CppPractice/codechef/Qualifying_to_Pre_Elimination.cpp
@@ -8,12 +8,15 @@ int main() {
     while (t--){
     lln n,k;
     cin>>n>>k;
+        //Initialising a vector
     vector<lln>S(n);
     for(lln i=0;i<n;i++) cin>>S[i];
-
-    sort(S.begin(),S.end(),greater<int>());
     
+        //Sorting the vector in decreasing order
+    sort(S.begin(),S.end(),greater<int>());
+         //min qualified teams will be equal to k
     lln ans=k;
+        //Iterating from kth to n-1 podition and check if adjacent element are equal then increment the ans variable by 1 else break
     for(lln i=k;i<n;i++){
         if(S[i]==S[k-1]){
             ans++;
